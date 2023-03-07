e2051389419bb92a21233954c948797e70a582c8
noah-fullerton <noahmfullerton@gmail.com>
2023-03-07 10:51:52 -0700

    idk bruh

--- activity_10_astronauts/sql/astronauts.sql
+++ activity_10_astronauts/sql/astronauts.sql
@@ -2,3 +2,33 @@
 -- Instructor: Thyago Mota
 -- Description: The astronauts database
 
+CREATE DATABASE astronauts;
+
+DROP TABLE Astronauts;
+
+CREATE TABLE Astronauts(
+    id SERIAL PRIMARY KEY,
+    lastName VARCHAR(40) NOT NULL,
+    firstName VARCHAR(40) NOT NULL,
+    suffix VARCHAR(5),
+    gender CHAR(1) NOT NULL,
+    birth DATE,
+    city VARCHAR(20),
+    state CHAR(30),
+    country VARCHAR(25),
+    status VARCHAR(30),
+    daysInSpace INT NOT NULL,
+    flights INT NOT NULL);
+
+--\copy Astronauts from /var/lib/postgresql/data/astronauts.csv
+--DEMILITER ',' CSV HEADER;
+
+--\copy astronauts from 'C:\Users\noahm\PycharmProjects\23SCS3810\postgres\learn\astronauts.csv'
+--WITH DELIMITER ',' CSV HEADER;
+
+--\copy Astronauts from /var/lib/postgresql/data/astronauts.csv
+--DELIMITER ',' CSV HEADER;
+
+\copy Astronauts (lastName, firstName, suffix, gender, birth, city, state, country, status, daysInSpace, flights) from C:\Users\noahm\PycharmProjects\23SCS3810\postgres\learn\astronauts.csv DELIMITER ',' CSV HEADER;
+
+
