4547ec53c0fe60c31dedc38338887774d811b9cb
noah-fullerton <noahmfullerton@gmail.com>
2023-03-09 11:49:59 -0700

    whoa that was hard

--- activity_11_employees/src/employees.sql
+++ activity_11_employees/src/employees.sql
@@ -3,10 +3,39 @@
 -- Description: The employees database
 
 -- TODO: create database employees
+CREATE TABLE Employees(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    sal DECIMAL(8,2) NOT NULL,
+    FOREIGN KEY (deptCode) REFERENCES Departments(code)
+);
 
 -- TODO: create table departments
+CREATE TABLE Employees(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    sal DECIMAL(8,2) NOT NULL,
+    deptCode CHAR(2),
+    FOREIGN KEY (deptCode) REFERENCES Departments(code)
+);
+
+CREATE TABLE Departments(
+    code PRIMARY KEY CHAR(2),
+    name VARCHAR(30)
+);
 
 -- TODO: populate table departments
+
+INSERT INTO Departments VALUES
+('HR', "Human Resources"),
+('IT', 'Information Technology'),
+('SL', 'Sales');
+
+INSERT INTO Employees (name, sal, deptCode) VALUES
+('Sam Mai Tai', 50000, 'HR');
+('John', 60000, 'HR');
+('Mary', 80000, 'IT')
+('Harry', 100, NULL)
 
 -- TODO: create table employees
 
@@ -14,21 +43,46 @@
 
 -- TODO: a) list all rows in Departments.
 
+SELECT * FROM Departments;
+
 -- TODO: b) list only the codes in Departments.
+
+SELECT code FROM Departments;
 
 -- TODO: c) list all rows in Employees.
 
+SELECT * FROM Employees;
+
 -- TODO: d) list only the names in Employees in alphabetical order.
+
+SELECT name FROM Employees ORDER BY 1;
 
 -- TODO: e) list only the names and salaries in Employees, from the highest to the lowest salary.
 
+SELECT name, salary FROM Employees ORDER BY 2 DESC;
+
 -- TODO: f) list the cartesian product of Employees and Departments.
+
+SELECT * FROM Employees, Departments;
 
 -- TODO: g) do the natural join of Employees and Departments; the result should be exactly the same as the cartesian product; do you know why?
 
+SELECT id, deptCode, code FROM Employees
+NATURAL JOIN Departments
+ORDER BY id, code;
+
 -- TODO: i) do an equi join of Employees and Departments matching the rows by Employees.deptCode and Departments.code (hint: use JOIN and the ON clause).
 
+SELECT * FROM Employees, Departments WHERE deptCode = code;
+
+
+
 -- TODO: j) same as previous query but project name and salary of the employees plus the description of their departments.
+
+SELECT name, salary, 'desc'
+FROM Employees A
+INNER JOIN Departments B
+ON A.deptCode = B.code;
 
 -- TODO: k) same as previous query but only the employees that earn less than 60000.
 
